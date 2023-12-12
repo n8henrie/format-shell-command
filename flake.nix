db6aaa0c6c03cb28eb39c1f708dd335956a6dddf
Nathan Henrie <nate@n8henrie.com>
2023-12-12 13:31:03 -0700

    Add nix stuff

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Flake for github.com/n8henrie/format-shell-command";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    inherit (nixpkgs) lib;
+    systems = ["aarch64-darwin" "x86_64-linux" "aarch64-linux"];
+    systemClosure = attrs:
+      builtins.foldl' (acc: system:
+        lib.recursiveUpdate acc (attrs system)) {}
+      systems;
+  in
+    systemClosure (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        inherit ((builtins.fromTOML (builtins.readFile ./Cargo.toml)).package) name;
+      in {
+        packages.${system} = {
+          default = self.packages.${system}.${name};
+          ${name} = pkgs.rustPlatform.buildRustPackage {
+            inherit name;
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+        };
+
+        apps.${system}.default = {
+          type = "app";
+          program = "${self.packages.${system}.${name}}/bin/${name}";
+        };
+
+        devShells.${system}.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            cargo-watch
+          ];
+        };
+      }
+    );
+}
